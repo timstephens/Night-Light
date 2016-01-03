0564e5b110cb82cc72552bf445d07be9c938b71a
Tim Stephens <tim@tjstephens.com>
2016-01-03 22:34:35 +0000

    Added colour fade logic
    Not tested and probably not working

--- LightMenu/LightMenu.ino
+++ LightMenu/LightMenu.ino
@@ -5,6 +5,7 @@
   31 December 2015
   User interface (and logic for an Arduino powered fading light alarm clock.
 */
+#include <Adafruit_NeoPixel.h>
 #include "tStruct.h"
 //typedef tStruct timeStruct;
 tStruct currentTime;  //global variable to hold the "current time" (when it was last checked)
@@ -17,6 +18,12 @@ enum mode {
   wakeupMode
 };
 mode opMode;
+
+#define PIN            7
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS      16
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
 
 
 void setup() {
@@ -69,8 +76,13 @@ void loop() {
     //Check elapsed time since wakeup mode start, and ensure that the fade is progressing properly
     //We'll use a granularity of 20s, which seems to be smooth with the rainbow effect,
     //and then use similar logic to get the brightness fade over.
-
+    /*
+    What needs to happen here is that the pattern needs to swap from one to another. 
+    Probably we need to have both patterns stored in arrays as Color values, and then 'simply' fade fron one to another. This may be a pain in the proverbial though. 
     //For each 20s, increase the brightness by FADETIME(s)/20s
+    //setColourFade (startColour, endColour, currentStep);
+    */
+    
 
   } else if (opMode == sleepMode) {
     //should be displaying the sleep pattern.
@@ -121,6 +133,8 @@ void setWakeup() {
   //Get the wakeup time from the user, and then set it into the global alarm time.
   //Wakeup should happen in a sequence of a few minutes after the alarm time that's set here.
   //We're initially going to fade up the lights
+  
+  //There's mileage in just setting the global to free a bit or RAM if necessary
   tStruct almTime;
   Serial.println("SET THE WAKEUP TIME");
   almTime = getTimeValue();
@@ -128,6 +142,7 @@ void setWakeup() {
   Serial.print("almTime");
   Serial.print(almTime.hours, DEC);
   Serial.println(almTime.mins, DEC);
+  alarmTime = almTime; 
 }
 
 tStruct getTimeValue() {
@@ -152,5 +167,48 @@ tStruct getTimeValue() {
   return myTimeStruct;
 }
 
+
+uint32_t setColourFade (uint32_t startColour, uint32_t endColour, int currentStep) {
+  //Do some computation to work out the current fade colour position, and then return it ready for setting to the strip. 
+  //For a first attempt, we just linearly fade from the first setpoint on each channel to the last. 
+  //Check out the Adafruit library for the definition of the Color member...
+  /*
+  Here's the line from the library that shows how it's converted:
+  
+  // Convert separate R,G,B into packed 32-bit RGB color.
+  // Packed format is always RGB, regardless of LED strand color order.
+  uint32_t Adafruit_NeoPixel::Color(uint8_t r, uint8_t g, uint8_t b) {
+    return ((uint32_t)r << 16) | ((uint32_t)g <<  8) | b;
+  }
+  
+  ALGORITHM
+ 
+  Unpack start and end colour into separate RGB members (as Uint8s)
+  Calculate colour step for RGB values round((start-end)/numSteps) (note that this may be negative value)
+  Assuming N steps, calculate the RGB values to return by multiplying colour step by current step number. 
+  
+  
+  */
+  
+  int numSteps = 32; //Number of different colours for the fade to happen over
+ 
+
+  //Unpack the Color values into bytes (from the ::setPixelColor() function). 
+  uint8_t
+      r = (uint8_t)(startColour >> 16),
+      g = (uint8_t)(startColour >>  8),
+      b = (uint8_t)startColour;
+  
+ uint8_t
+      r1 = (uint8_t)(endColour >> 16),
+      g1 = (uint8_t)(endColour >>  8),
+      b1 = (uint8_t)endColour;
+  
+  r=round(((r-r1)/numSteps)*currentStep);  //Overwrite the r, g, b, values with the new ones.
+  g=round(((g-g1)/numSteps)*currentStep);
+  b=round(((b-b1)/numSteps)*currentStep);
+  
+  return strip.Color(r,g,b);
+}
 
 
